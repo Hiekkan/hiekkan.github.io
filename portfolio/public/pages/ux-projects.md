84f2f798988c560402639c48c9e12956bc4bb43a
Hiekkan <20253935+Hiekkan@users.noreply.github.com>
2024-07-11 21:21:43 +0200

    First state

---
+++ portfolio/public/pages/ux-projects.md
@@ -0,0 +1,13 @@
+# UX Projects
+
+In this page I present some of my projects of my studies
+
+## Finland Travelplanner
+
+## Nike Rebranding
+
+## Paldeck (WIP)
+
+## Prediploma Fitschi Ski
+
+&nbsp;
--- portfolio/src/app/pages/pages.ts
+++ portfolio/src/app/pages/pages.ts
@@ -1,4 +1,5 @@
 export const pages = [
     { index: 0, name: 'overview.md', route: '/overview' },
-    { index: 1, name: 'projects.md', route: '/projects' },
+    { index: 1, name: 'code-projects.md', route: '/code-projects' },
+    { index: 2, name: 'ux-projects.md', route: '/ux-projects'  }
 ]
