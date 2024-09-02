cb305d5987cb4f992cd816f84d9bb4bb77588e23
Hiekkan <20253935+Hiekkan@users.noreply.github.com>
2024-09-02 21:52:53 +0200

    Fix some small issues

--- portfolio/src/app/pages/pages.ts
+++ portfolio/src/app/pages/pages.ts
@@ -1,5 +1,5 @@
 export const pages = [
     { index: 0, name: 'overview.md', route: '/overview' },
-    { index: 1, name: 'code-projects.md', route: '/code-projects' },
-    { index: 2, name: 'ux-projects.md', route: '/ux-projects'  }
+    { index: 1, name: 'code.md', route: '/code' },
+    { index: 2, name: 'ux.md', route: '/ux'  }
 ]
